3aef1ab41b95bdb7493b04a085cf2fa18ec4df97
zwpride <zwpride@163.com>
2021-06-17 16:07:00 +0800

    final version

--- Android-Mail-master/android/app/src/main/java/edu/hnu/mail/constant/DefaultServer.java
+++ Android-Mail-master/android/app/src/main/java/edu/hnu/mail/constant/DefaultServer.java
@@ -2,10 +2,10 @@ package edu.hnu.mail.constant;
 
 public class DefaultServer {
 
-    public static final String ADMIN_HOST = "http://10.68.147.56:8080";
-    public static final String SMTP_HOST = "10.68.147.56";
+    public static final String ADMIN_HOST = "http://10.68.103.139:8080";
+    public static final String SMTP_HOST = "10.68.103.139";
     public static final int SMTP_PORT = 25;
-    public static final String POP_HOST = "10.68.147.56";
+    public static final String POP_HOST = "10.68.103.139";
     public static final int POP_PORT = 110;
 
     public static boolean isFetching = false;
